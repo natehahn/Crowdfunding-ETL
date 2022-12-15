3ee9ed84fc3baced6831225e4fe25de7476bf13c
natehahn <nate.hahn@comcast.net>
2022-12-14 21:35:29 -0700

    Added final code

--- Crowdfunding_SQL_Analysis.sql
+++ Crowdfunding_SQL_Analysis.sql
@@ -26,8 +26,10 @@ SELECT contacts.first_name,
 	contacts.last_name,
 	contacts.email,
 	campaign.goal - campaign.pledged AS "Remaining Goal Amount"
--- INTO email_contacts_remaining_goal_amount
-FROM campaign, contacts
+INTO email_contacts_remaining_goal_amount
+FROM campaign
+INNER JOIN contacts
+ON campaign.contact_id = contacts.contact_id
 WHERE campaign.outcome = 'live'
 ORDER BY "Remaining Goal Amount" DESC;
 
@@ -39,9 +41,23 @@ SELECT * FROM email_contacts_remaining_goal_amount;
 -- Create a table, "email_backers_remaining_goal_amount" that contains the email address of each backer in descending order, 
 -- and has the first and last name of each backer, the cf_id, company name, description, 
 -- end date of the campaign, and the remaining amount of the campaign goal as "Left of Goal". 
+SELECT backers.email,
+       backers.first_name,
+	   backers.last_name,
+	   backers.cf_id,
+	   campaign.company_name,
+	   campaign.description,
+	   campaign.end_date,
+	   campaign.goal-campaign.pledged AS "Left of Goal"
+INTO email_backers_remaining_goal_amount
+FROM backers
+INNER JOIN campaign
+ON backers.cf_id = campaign.cf_id
+WHERE outcome = 'live'
+ORDER BY backers.last_name ASC;
 
 
 
 -- Check the table
-
+SELECT * FROM email_backers_remaining_goal_amount;
 
